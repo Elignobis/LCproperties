babe292a81868aee41c93e00260e37a7d82e9ad2
Sibongile <rwayisibongile@gmail.com>
2022-07-12 11:01:13 +0200

    commit

--- scripts/admin.js
+++ scripts/admin.js
@@ -1,39 +1,36 @@
-const properties = document.querySelector('propoertiesform');
-const input = document.querySelector('input');
-const ul = document.querySelector('propertyList');
-
-// Create
-
-
-function createLi () {
-    const li = document.createElement('li');
-    const span = document.createElement('span');
-    span.textContent = input.value;
-    const label = document.createElement('label');
-    label.textContent = 'confirmed';
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox'
-    const editBtn = document.createElement('button');
-    editBtn.textContent = 'edit';
-    const removeBtn = document.createElement('button');
-    removeBtn.textContent = 'remove';
-
-    li.appendChild(span);
-    li.appendChild(label);
-    li.appendChild(checkbox);
-    li.appendChild(editBtn);
-    li.appendChild(removeBtn);
-
-return li;
+function onFormSubmit() {
+    let formData = readFormData();
+    insertNewProperty(formData);
 }
 
+function readFormData() {
+    let formData = {};
+    formData["propImg"] = document.getElementById("propImg").value;
+    formData["propCode"] = document.getElementById("propCode").value;
+    formData["propType"] = document.getElementById("propType").value;
+    formData["propAddress"] = document.getElementById("propAddress").value;
+    formData["propSize"] = document.getElementById("propSize").value;
+    formData["propPrice"] = document.getElementById("propPrice").value;
+    return formData
+}
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    
-    const li = createLi();
+function insertNewProperty(data) {
+    let table = document.getElementById("propertyList").getElementsByTagName('tbody')[0];
+    let newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.propImg;
+    cell2 = newRow.insertCell(1);
+    cell2.innerHTML = data.propCode;
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.propType;
+    cell4 = newRow.insertCell(3);
+    cell4.innerHTML = data.propAddress;
+    cell5 = newRow.insertCell(4);
+    cell5.innerHTML = data.propSize;
+    cell6 = newRow.insertCell(5);
+    cell6.innerHTML = data.propPrice;
+    cell7 = newRow.insertCell(6);
+    cell7.innerHTML = `<a>Edit</a> 
 
-    if(input.value === '') {
-        alert('Please enter property')
-    }
-})
+                        <a>Delete</a>`;
+}
